451696cabd022808aa1c9cb0d7d2ff3afe917a00
Travon <zhouyanbai@qq.com>
2021-12-04 11:07:48 +0800

    update

--- game.py
+++ game.py
@@ -1,19 +1,82 @@
 from tkinter import Frame, Label, CENTER
-import logic
+from logic import Space
+import constants as c
+from functools import partial
 
 
-class GameGrid(Frame):
+class GameGrid(Frame, Space):
     def __init__(self):
-        super().__init__()
+        Frame.__init__(self)
+        Space.__init__(self)
 
         self.grid()
         self.master.title('MoveAtoms')
-        self.master.bind('<Key>', self.key_down)
+        # self.master.bind('<ButtonRelease-1>', self.key_down)
+
+        # self.space = Space()
+        self.space = []
 
         self.commands = {
-
+            '<ButtonRelease-1>': self.separate,
+            '<ButtonRelease-3>': self.merge
         }
+        self.draw_grid()
+        self.master.mainloop()
 
-        self.grid = []
-        self.init_grid()
-        
+    def draw_grid(self):
+        for item in self.space:
+            # print(item)
+            item.destroy()
+        HSIZE = (c.GRID_SIZE + c.GRID_PADDING) * self.length + c.GRID_PADDING
+        VSIZE = c.GRID_SIZE + 2 * c.GRID_PADDING
+        background = Frame(self,
+                           bg=c.BACKGROUND_COLOR_GAME,
+                           width=HSIZE,
+                           height=VSIZE)
+        background.grid()
+        self.space.append(background)
+
+        for i in range(self.length):
+            amount = self.status[i]
+            cell = Frame(background,
+                         bg=c.BACKGROUND_COLOR_DICT[amount],
+                         width=c.GRID_SIZE,
+                         height=c.GRID_SIZE)
+
+            # cell.bind('<Key>', self.key_down)
+            # cell.bind('<ButtonRelease-1>', partial(self.separate, i=i))
+            cell.grid(row=0,
+                      column=i,
+                      padx=c.GRID_PADDING,
+                      pady=c.GRID_PADDING)
+            t = Label(master=cell,
+                      text=str(amount),
+                      bg=c.BACKGROUND_COLOR_DICT[amount],
+                      fg=c.CELL_COLOR_DICT[amount],
+                      justify=CENTER,
+                      font=c.FONT,
+                      width=1,
+                      height=1)
+            for key, opt in self.commands.items():
+                t.bind(key, partial(opt, i=i))
+            # t.bind('<ButtonRelease-3>', self.key_down)
+            t.grid()
+            self.space.append(cell)
+
+    def key_down(self, event):
+        print(event)
+        key = event.keysym
+        if key == c.KEY_QUIT: exit()
+
+    def separate(self, event, i):
+        print(event)
+        super().separate(i)
+        self.draw_grid()
+    
+    def merge(self, event, i):
+        print(event)
+        super().merge(i)
+        self.draw_grid()
+
+# print(GameGrid.__mro__)
+game_grid = GameGrid()
--- logic.py
+++ logic.py
@@ -1,13 +1,20 @@
-import numpy as np
+from numpy import zeros, pad, array, uint8
 
 class Space:
-    # def __init__(self):
-    #     self.space = 
+    def __init__(self):
+        self.status = zeros(9, dtype=uint8)
+        self.status[1] = 1
     #     pass
     def separate(self, i):
         if self.status[i]:
+            if i == 0:
+                self.status = pad(self.status, (1,0), 'constant', constant_values=(0,0))
+            elif i == self.length - 1:
+                self.status = pad(self.status, (0,1), 'constant', constant_values=(0,0))
             # try:
-            self.status[i-1:i+1] += np.array([1,-1,1])
+            # print(f"i:{i}, {self.status[i-1:i+1]}")
+            else:
+                self.status[i-1:i+2] += array([1,-1,1], dtype=uint8)
             # except ValueError and self.status[i-1:i+1].shape != (3,):
             #     try:
             #         statusa[]
@@ -15,5 +22,9 @@ class Space:
         pass
     def merge(self, i):
         if self.status[i-1] and self.status[i+1]:
-            self.status[i-1:i+1] += np.array([-1,1,-1])
+            self.status[i-1:i+2] += array([-1,1,-1], dtype=uint8)
         pass
+    
+    @property
+    def length(self):
+        return len(self.status)
